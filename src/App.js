42bc7bdde0361e920e0009047f10d48406f560cc
Reeshon <reeshonrs@gmail.com>
2025-01-28 19:27:28 +0200

    gfxc

--- src/App.js
+++ src/App.js
@@ -2,19 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/Home';
 import Products from './components/Products';
-import Contact from './Contact';
 import Cart from './components/Cart';
 import Wishlist from './components/Wishlist';
 import Search from './components/Search';
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/products" component={Products} />
-          <Route path="/contact" component={Contact} />
           <Route path="/cart" component={Cart} />
           <Route path="/wishlist" component={Wishlist} />
           <Route path="/search" component={Search} />
